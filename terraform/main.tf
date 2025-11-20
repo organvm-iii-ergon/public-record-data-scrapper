430bda06c4ed3e191474bd2af7c23cc7ff1c6945
_]| |{::::4_ivi374::}~,.'; • ;'.,~{::4_8100ivi::::}|| [___ <etceter4@etceter4.com>
2025-11-20 02:58:50 -0500

    Fix CloudWatch alarm numeric parameters

--- terraform/main.tf
+++ terraform/main.tf
@@ -413,12 +413,12 @@ resource "aws_sns_topic_subscription" "alerts_email" {
 resource "aws_cloudwatch_metric_alarm" "rds_cpu" {
   alarm_name          = "${local.name_prefix}-rds-high-cpu"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "2"
+  evaluation_periods  = 2
   metric_name         = "CPUUtilization"
   namespace           = "AWS/RDS"
-  period              = "300"
+  period              = 300
   statistic           = "Average"
-  threshold           = "80"
+  threshold           = 80
   alarm_description   = "This metric monitors RDS CPU utilization"
   alarm_actions       = [aws_sns_topic.alerts.arn]
 
@@ -432,12 +432,12 @@ resource "aws_cloudwatch_metric_alarm" "rds_cpu" {
 resource "aws_cloudwatch_metric_alarm" "rds_storage" {
   alarm_name          = "${local.name_prefix}-rds-low-storage"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = 1
   metric_name         = "FreeStorageSpace"
   namespace           = "AWS/RDS"
-  period              = "300"
+  period              = 300
   statistic           = "Average"
-  threshold           = "10737418240" # 10 GB in bytes
+  threshold           = 10737418240 # 10 GB in bytes
   alarm_description   = "This metric monitors RDS free storage space"
   alarm_actions       = [aws_sns_topic.alerts.arn]
 
@@ -452,12 +452,12 @@ resource "aws_cloudwatch_metric_alarm" "rds_storage" {
 resource "aws_cloudwatch_metric_alarm" "redis_cpu" {
   alarm_name          = "${local.name_prefix}-redis-high-cpu"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "2"
+  evaluation_periods  = 2
   metric_name         = "CPUUtilization"
   namespace           = "AWS/ElastiCache"
-  period              = "300"
+  period              = 300
   statistic           = "Average"
-  threshold           = "75"
+  threshold           = 75
   alarm_description   = "This metric monitors Redis CPU utilization"
   alarm_actions       = [aws_sns_topic.alerts.arn]
 
@@ -471,12 +471,12 @@ resource "aws_cloudwatch_metric_alarm" "redis_cpu" {
 resource "aws_cloudwatch_metric_alarm" "redis_memory" {
   alarm_name          = "${local.name_prefix}-redis-high-memory"
   comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "2"
+  evaluation_periods  = 2
   metric_name         = "DatabaseMemoryUsagePercentage"
   namespace           = "AWS/ElastiCache"
-  period              = "300"
+  period              = 300
   statistic           = "Average"
-  threshold           = "80"
+  threshold           = 80
   alarm_description   = "This metric monitors Redis memory usage"
   alarm_actions       = [aws_sns_topic.alerts.arn]
 
